b49c254385ef3666cdb9783d405c84fb87aa7451
hassaanakram <hassaan.akram.ha@gmail.com>
2022-04-16 00:24:46 +0500

    thz channel written. NO IDEA IF THE ARRAY SHAPES ARE GONNA MATHC. WILL CHECK LATER

--- OurEnvironment.py
+++ OurEnvironment.py
@@ -1,3 +1,4 @@
+from math import dist
 import numpy as np
 from numpy import math as m
 import scipy.stats as s
@@ -55,12 +56,15 @@ class mmWaveChannel:
 
 class THzChannel:
 	def __init__(self):
-		self.frequency = 2.4
-		self.bandwidth = 20E6
-		self.alpha = 0.5
-		self.shadow_mean = 0
-		self.shadow_std = 4
-		self.beta = 3
+		self.frequency = 3E11
+		self.bandwidth = 10E9
+		self.kF = 0.0033 # some coefficient that rn i'm too tired to read about. the units are m-1
+
+	def path_loss_absorption(self, distance):
+		return self.kF*distance*10*m.log10(m.exp(1))
+
+	def path_loss_spread(self, distance):
+		return 20*m.log10((4*pi*self.frequency*d)/c)
 
 	def get_path_loss(self, position_BS, position_UE, shadowing):
 		# Positions should be a tuple (x,y) with each x,y being numpy arrays of Kx1 where K is the 
@@ -68,8 +72,7 @@ class THzChannel:
 		BS_x, BS_y = position_BS
 		UE_x, UE_y = position_UE
 		num_UE = UE_x.shape[0]
-		shadowing = s.lognorm.rvs(s=self.shadow_std, size=num_UE)
 		distance = m.sqrt(m.pow(BS_x-UE_x,2) - m.pow(BS_y-UE_y,2))
-		path_loss = 10*m.log10((4*m.pi*self.frequency)/c) + 10*self.beta*m.log10(distance)+shadowing
+		path_loss = self.path_loss_absorption(distance) + self.path_loss_spread(distance)
 
 		return path_loss
